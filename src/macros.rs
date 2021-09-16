325dd0c40269e8225a471bd1fa1d9802b9f62da6
Jakub Arnold <darthdeus@gmail.com>
2021-09-16 20:48:44 +0200

    Trailing comma in iff!

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! iff {
-    ($cond:expr, $a:expr, $b:expr) => {
+    ($cond:expr, $a:expr, $b:expr $(,)?) => {
         Behavior::If(
             stringify!($cond).to_string(),
             // Box::new($cond),
