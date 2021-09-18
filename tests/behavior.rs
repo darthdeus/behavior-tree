23db9b4f7bbad8cdfb88c2ced07d5a469cf9ef73
Jakub Arnold <darthdeus@gmail.com>
2021-09-18 08:55:31 +0200

    Better test for sequence

--- tests/behavior.rs
+++ tests/behavior.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, rc::Rc};
+
 use behavior_tree::*;
 
 struct Counter {
@@ -63,9 +65,32 @@ fn test_simple_sequence() {
 
 #[test]
 fn test_stateful_action() {
+    struct CounterWrap {
+        value: Rc<RefCell<i32>>,
+    }
+
+    impl StatefulAction<()> for CounterWrap {
+        fn tick(&mut self, _data: &mut ()) -> Status {
+            let mut value = self.value.borrow_mut();
+
+            if *value == 0 {
+                *value += 1;
+                Status::Running
+            } else {
+                Status::Success
+            }
+        }
+    }
+
+    let v1 = Rc::new(RefCell::new(0));
+    let v2 = Rc::new(RefCell::new(0));
+
+    let c1 = Box::new(CounterWrap { value: v1.clone() });
+    let c2 = Box::new(CounterWrap { value: v2.clone() });
+
     let mut bt: Behavior<()> = sequence![
-        Behavior::StatefulAction("inc_x".to_owned(), Box::new(Counter { value: 0 })),
-        Behavior::StatefulAction("inc_y".to_owned(), Box::new(Counter { value: 0 }))
+        Behavior::StatefulAction("inc_x".to_owned(), c1),
+        Behavior::StatefulAction("inc_y".to_owned(), c2)
     ];
 
     //   Player visible?
@@ -77,32 +102,29 @@ fn test_stateful_action() {
     // sequence(start_attack, wait, stop_attack)
 
     let mut data = ();
+    assert_eq!(*v1.borrow(), 0);
+    assert_eq!(*v2.borrow(), 0);
 
     let (status, debug_repr) = bt.tick(0.0, &mut data);
-    // dbg!(&debug_repr);
-    // assert_eq!(data.x, 1);
-    // assert_eq!(data.y, 0);
     assert_eq!(status, Status::Running);
     assert_eq!(debug_repr.cursor.index(), 0);
+    assert_eq!(*v1.borrow(), 1);
+    assert_eq!(*v2.borrow(), 0);
 
     let (status, debug_repr) = bt.tick(0.0, &mut data);
-    // dbg!(&debug_repr);
-    // assert_eq!(data.x, 1);
-    // assert_eq!(data.y, 1);
     assert_eq!(status, Status::Running);
     assert_eq!(debug_repr.cursor.index(), 1);
+    assert_eq!(*v1.borrow(), 1);
+    assert_eq!(*v2.borrow(), 1);
 
     let (status, debug_repr) = bt.tick(0.0, &mut data);
-    // dbg!(&debug_repr);
-    // assert_eq!(data.x, 1);
-    // assert_eq!(data.y, 1);
     assert_eq!(status, Status::Success);
     assert_eq!(debug_repr.cursor.index(), 2);
+    assert_eq!(*v1.borrow(), 1);
+    assert_eq!(*v2.borrow(), 1);
 
     // let (status, debug_repr) = bt.tick(0.0, &mut data);
-    // dbg!(&debug_repr);
-    // assert_eq!(data.value, 4);
-    // assert_eq!(status, Status::Success);
+    // assert_eq!(status, Status::Running);
     // assert_eq!(debug_repr.cursor.index(), 1);
 }
 
